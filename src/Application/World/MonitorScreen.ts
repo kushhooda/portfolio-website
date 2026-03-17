9f64f4f5d5363e60142f5a21d9396e130d64b8a2
kushhooda <kush9hooda@gmail.com>
2026-03-17 13:12:16 +0300

    Point monitor to Vercel inner site

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://bykush.dev/';
+        iframe.src = 'https://portfolio-inner-site-ho321nc9j-kushhoodas-projects.vercel.app/';
         /**
          * Use dev server is query params are present
          *
